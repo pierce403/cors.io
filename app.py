f2418730beffb127b8478cb3d1e6feedf8e97af0
Dean Pierce <pierce403@gmail.com>
2018-04-18 18:18:01 -0700

    hmm

--- app.py
+++ app.py
@@ -1,63 +1,32 @@
-"""
-Flask Documentation:     http://flask.pocoo.org/docs/
-Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
-Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
+from flask import render_template
+from flask import request
 
-This file creates your application.
-"""
-
-import os
-from flask import Flask, render_template, request, redirect, url_for
-
+import time
+import flask
+from flask import Flask
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
-
-
-###
-# Routing for your application.
-###
+import requests
 
 @app.route('/')
-def home():
-    return "sup"
-    #"""Render website's home page."""
-    #return render_template('home.html')
+def index():
+  print("potato")
 
+  qs=request.query_string
 
-@app.route('/about/')
-def about():
-    """Render the website's about page."""
-    return render_template('about.html')
+  if qs:
+    try:
+      agent = request.headers.get('User-Agent')
+      user_agent = {'User-agent': agent}
+      rt = requests.get(qs.decode('utf8'), headers = user_agent).text
+    except:
+      rt = "nope"
 
-
-###
-# The functions below should be applicable to all Flask apps.
-###
-
-@app.route('/<file_name>.txt')
-def send_text_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
-
-
-@app.after_request
-def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=600'
+    response = flask.Response(rt)
+    response.headers['Access-Control-Allow-Origin'] = '*'
     return response
+  else:
+    print("nope")
 
+  return render_template('index.html')
 
-@app.errorhandler(404)
-def page_not_found(error):
-    """Custom 404 page."""
-    return render_template('404.html'), 404
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
